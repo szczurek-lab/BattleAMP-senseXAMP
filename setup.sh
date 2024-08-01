e2063df6c5285d0c69a2bdc713203cb05fbedcb1
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2024-08-01 10:09:50 +0200

    build (env): loosen requirements and create setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+conda install pytorch==1.7.1 -c pytorch
+
+pip install -r requirements.txt
+
+python -c "import gdown; gdown.download_folder(url=\
+'https://drive.google.com/drive/folders/1wNuoFrFZd3q3AlGyV-s2WpaVMs06N4L1',\
+output='weights')"
+
+mkdir -p datasets/ori_datasets
+mkdir -p datasets/esm_embeddings
+mkdir -p datasets/stc_info
